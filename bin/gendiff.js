9d4492301f977058c3d4e3518feeb8422d3143bf
Nadezhda Kamenskaya <nady.kamenskaya@gmail.com>
2022-12-09 17:39:49 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,11 @@ const program = new Command();
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
-
+    .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format');
+    
 program.parse();
+
+// console.log(`${program.opts().format}`);
