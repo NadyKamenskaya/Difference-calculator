ad91347c243e24815b3d5fbece0d38a4c52e1071
Nadezhda Kamenskaya <nady.kamenskaya@gmail.com>
2022-12-14 14:23:59 +0300

    Add file1.json, file2.json, index.js, update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .arguments('<filepath1> <filepath2>')
-    .helpOption('-h, --help', 'output usage information')
-    .option('-f, --format <type>', 'output format');
-    
-program.parse();
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => genDiff((filepath1), (filepath2)));
 
-// console.log(`${program.opts().format}`);
+program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { resolve } from 'path';
+import { cwd } from 'process';
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+
+export default (path1, path2) => {
+  const data1 = JSON.parse(readFileSync(resolve(cwd(), path1)));
+  const data2 = JSON.parse(readFileSync(resolve(cwd(), path2)));
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+
+  const unionKeys = _.sortBy(_.union(keys1, keys2));
+
+  const objectDiff = unionKeys.reduce((acc, key) => {
+    if (keys1.includes(key) && keys2.includes(key)) {
+      if (data1[key] === data2[key]) {
+        return { ...acc, [`  ${key}`]: data1[key] };
+      }
+
+      return { ...acc, [`- ${key}`]: data1[key], [`+ ${key}`]: data2[key] };
+    }
+    if (keys1.includes(key) && !keys2.includes(key)) {
+      return { ...acc, [`- ${key}`]: data1[key] };
+    }
+
+    return { ...acc, [`+ ${key}`]: data2[key] };
+  }, {});
+
+  const result = Object.entries(objectDiff).reduce((acc, [key, value]) => `${acc}\n  ${key}: ${value}`, '');
+
+  console.log(`{${result}\n}`);
+};
