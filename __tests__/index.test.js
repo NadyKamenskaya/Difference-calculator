50748eb3a6fb1f25b958ec9ff1daf3bd792c499c
Nadezhda Kamenskaya <nady.kamenskaya@gmail.com>
2022-12-16 23:39:12 +0300

    Add __tests__/

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@jest/globals';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test('check files comparison', () => {
+  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+
+  expect(actual).toEqual(readFile('expected_file.json'));
+});
