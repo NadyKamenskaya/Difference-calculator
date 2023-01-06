f458b785ccf0d381c836bc3138a0b59c51478bd2
Nadezhda Kamenskaya <nady.kamenskaya@gmail.com>
2023-01-07 02:12:40 +0300

    Add new files and test for new format plain

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,23 +4,34 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import yaml from 'js-yaml';
 import genDiff, { parsers } from '../src/index.js';
-import stylish from '../src/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('check json comparison', () => {
-  const actual = stylish(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')));
+test('check json comparison stylish', () => {
+  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
 
-  expect(actual).toEqual(readFile('expected_file.json'));
+  expect(actual).toEqual(readFile('expected_file_stylish.json'));
 });
 
-test('check yaml comparison', () => {
-  const actual = stylish(genDiff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml')));
+test('check yaml comparison stylish', () => {
+  const actual = genDiff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'), 'stylish');
 
-  expect(actual).toEqual(readFile('expected_filepath.yml'));
+  expect(actual).toEqual(readFile('expected_filepath_stylish.yml'));
+});
+
+test('check json comparison plain', () => {
+  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
+
+  expect(actual).toEqual(readFile('expected_file_plain.json'));
+});
+
+test('check yaml comparison plain', () => {
+  const actual = genDiff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'), 'plain');
+
+  expect(actual).toEqual(readFile('expected_filepath_plain.yml'));
 });
 
 test('parsers json', () => {
