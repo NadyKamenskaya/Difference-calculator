31f8827ed9a418f26d24e3a5e767f0fd72495d1e
Nadezhda Kamenskaya <nady.kamenskaya@gmail.com>
2023-01-21 20:57:02 +0300

    Update buildDiffTree.js

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -7,26 +7,22 @@ const buildDiffTree = (data1, data2) => {
     const unionKeys = _.sortBy(_.union(keys1, keys2));
 
     const result = unionKeys.map((key) => {
-      const value1 = obj1[key];
-      const value2 = obj2[key];
-
-      if (keys1.includes(key) && keys2.includes(key)) {
-        if (_.isObject(value1) && _.isObject(value2)) {
-          return { name: key, type: 'nested', value: iter(value1, value2) };
-        }
-        if (value1 === value2) {
-          return { name: key, type: 'unchanged', value: value1 };
-        }
-
-        return {
-          name: key, type: 'changed', valueBefore: value1, valueAfter: value2,
-        };
+      if (!_.has(obj1, key)) {
+        return { name: key, type: 'added', value: obj2[key] };
       }
-      const newAcc = (keys1.includes(key) && !keys2.includes(key))
-        ? { name: key, type: 'deleted', value: value1 }
-        : { name: key, type: 'added', value: value2 };
+      if (!_.has(obj2, key)) {
+        return { name: key, type: 'deleted', value: obj1[key] };
+      }
+      if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+        return { name: key, type: 'nested', value: iter(obj1[key], obj2[key]) };
+      }
+      if (obj1[key] === obj2[key]) {
+        return { name: key, type: 'unchanged', value: obj1[key] };
+      }
 
-      return newAcc;
+      return {
+        name: key, type: 'changed', value1: obj1[key], value2: obj2[key],
+      };
     });
 
     return result;
