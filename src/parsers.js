54dbf21b4495d0dde6a5d2eca27d8f87faf5d9a9
Nadezhda Kamenskaya <nady.kamenskaya@gmail.com>
2023-01-21 20:58:11 +0300

    Update parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -5,6 +5,7 @@ const parsers = (data, extension) => {
     case 'json':
       return JSON.parse(data);
     case 'yml':
+    case 'yaml':
       return yaml.load(data);
     default:
       throw new Error(`Unknown extension: '${extension}'!`);
