3fba381fef8412cf0d44ca6eca3dd2f99af9e860
Nadezhda Kamenskaya <nady.kamenskaya@gmail.com>
2023-01-22 13:38:04 +0300

    Fix parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,14 @@
 import yaml from 'js-yaml';
 
-const parsers = (data, extension) => {
-  switch (extension) {
+const parsers = (data, parserType) => {
+  switch (parserType) {
     case 'json':
       return JSON.parse(data);
     case 'yml':
     case 'yaml':
       return yaml.load(data);
     default:
-      throw new Error(`Unknown extension: '${extension}'!`);
+      throw new Error(`Unknown extension: '${parserType}'!`);
   }
 };
 
